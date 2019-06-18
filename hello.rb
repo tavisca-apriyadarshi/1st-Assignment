f43ef6b5ee0b3943331e1a017bca1d4570aa6f1d
tavisca-apriyadarshi <apriyadarshi@tavisca.in>
2019-06-18 15:17:43 +0530

    Added a default value

--- hello.rb
+++ hello.rb
@@ -1,2 +1,6 @@
-puts "Hello, World"
-puts "Hello, #{ARGV.first}!, How are you?"
+#puts "Hello, World"
+#puts "Hello, #{ARGV.first}!, How are you?"
+
+name = ARGV.first || "SomeOne"
+
+puts "Hello, #{name}!"
